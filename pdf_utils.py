200b3e4ef2ca8083a6870f543b4f0fc35a4353a1
Akash Chandru613 <akashchandru613@gmail.com>
2025-05-31 18:53:18 -0800

    Fix: updated folder and dependency issues

--- pdf_utils.py
+++
@@ -1,9 +0,0 @@
-# pdf_utils.py
-import PyPDF2
-
-def extract_text_from_pdf(pdf_file):
-    pdf_reader = PyPDF2.PdfReader(pdf_file)
-    text = ""
-    for page in pdf_reader.pages:
-        text += page.extract_text() or ""
-    return text.strip()
